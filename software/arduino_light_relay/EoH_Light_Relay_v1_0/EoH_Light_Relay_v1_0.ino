8d9224653f9f13f8f0130cbc90b2054bcad1da41
berro78 <berro78@gmail.com>
2015-04-19 21:15:30 +0200

    Arduino Light Relay

---
+++ software/arduino_light_relay/EoH_Light_Relay_v1_0/EoH_Light_Relay_v1_0.ino
@@ -0,0 +1,43 @@
+//BLE Light Control for Eye of Horus interface
+
+const byte pin_relay = 13;
+
+String sp_read()
+{
+  String serialLine = "";
+  while (Serial.available() >0)  
+  {
+    serialLine = serialLine + char(Serial.read());
+  }
+  return serialLine;
+}
+
+void setup()
+{
+  pinMode (pin_relay, OUTPUT); //Relay
+  digitalWrite(pin_relay,LOW);
+  
+  Serial.begin(57600);
+  delay(2000);
+}
+
+void loop()
+{ 
+  String msg = sp_read();
+
+  if(msg.indexOf("light_on") >= 0)
+  {
+    digitalWrite(pin_relay,HIGH);
+    Serial.println("relay_on");
+    delay(50);
+  }
+  else if(msg.indexOf("light_off") >= 0)
+  {
+    digitalWrite(pin_relay,LOW);
+    Serial.println("relay_off");
+    delay(50);
+  }
+  // Es necesario para que lea bien el puerto
+  delay(100);
+}
+
